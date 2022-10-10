8484ae4e72c79e372c2a9a4e40a93ff75d6dc065
golikovartyom <golikovartem404@gmail.com>
2022-10-10 13:30:57 +0400

    Add little fixes and comment

--- GCD-HomeWork-BruteForce/Sources/Extensions/String + Extension.swift
+++ GCD-HomeWork-BruteForce/Sources/Extensions/String + Extension.swift
@@ -8,14 +8,13 @@
 import Foundation
 
 extension String {
+    
     var digits:      String { return "0123456789" }
     var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
     var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
     var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
     var letters:     String { return lowercase + uppercase }
     var printable:   String { return digits + letters + punctuation }
-
-
 
     mutating func replace(at index: Int, with character: Character) {
         var stringArray = Array(self)
